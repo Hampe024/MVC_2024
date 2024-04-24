2095792cf65a7ec72d6e7c4323d237cdff419720
Hampus <Hampe024@gmail.com>
2024-04-24 22:01:33 +0200

    report done

---
+++ src/Controller/Lucky.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Lucky extends AbstractController
+{
+    #[Route("/lucky", name: "lucky")]
+    public function lucky(): Response
+    {
+        $number = random_int(0, 100);
+
+        $data = [
+            'number' => $number
+        ];
+
+        return $this->render('misc/lucky.html.twig', $data);
+    }
+}
---
+++ src/Controller/Report.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Report extends AbstractController
+{
+    #[Route("/", name: "me")]
+    public function me(): Response
+    {
+        return $this->render('report/me.html.twig');
+    }
+    #[Route("/about", name: "about")]
+    public function about(): Response
+    {
+        return $this->render('report/about.html.twig');
+    }
+    #[Route("/report", name: "report")]
+    public function report(): Response
+    {
+        return $this->render('report/report.html.twig');
+    }
+}
