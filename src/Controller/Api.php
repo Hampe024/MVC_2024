b29834d8acd048f67fa1d4483cc5b6f7df14da49
Hampus <Hampe024@gmail.com>
2024-04-26 18:51:15 +0200

    added API requirements

--- src/Controller/Api.php
+++ src/Controller/Api.php
@@ -2,10 +2,14 @@
 
 namespace App\Controller;
 
+use App\Card\DeckOfCards;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class Api extends AbstractController
 {
@@ -14,6 +18,7 @@ class Api extends AbstractController
     {
         return $this->render('misc/api.html.twig');
     }
+
     #[Route("/api/quote", name: "quote")]
     public function quote(): Response
     {
@@ -39,6 +44,76 @@ class Api extends AbstractController
             "time" => date("h:i:sa"),
         ];
 
+        return new JsonResponse($data);
+    }
+
+    private function getDeck(
+        SessionInterface $session
+        ): DeckOfCards
+    {
+        $deck = $session->has('deck') ? $session->get('deck') : new DeckOfCards();
+
+        $session->set('deck', $deck);
+
+        return $deck;
+    }
+
+    #[Route("/api/deck", name: "apiDeck")]
+    public function apiDeck(
+        SessionInterface $session
+    ): Response
+    {
+        $deck = $this->getDeck($session);
+        $data = [
+            'deck' => $deck->getDeckAsString(),
+        ];
+
+        return new JsonResponse($data);
+    }
+
+    #[Route("/api/deck/shuffle", name: "apiDeckShuffle")]
+    public function apiDeckShuffle(
+        SessionInterface $session
+    ): Response
+    {
+        $deck = $this->getDeck($session);
+
+        $deck->shuffle();
+
+        $session->set('deck', $deck);
+
+        $data = [
+            'deck' => $deck->getDeckAsString(),
+        ];
+
+        return new JsonResponse($data);
+    }
+
+    #[Route("/api/deck/draw", name: "apiDeckDrawOne")]
+    public function apiDeckDrawOne(
+    ): Response
+    {
+        return $this->redirectToRoute('apiDeckDraw', ['num' => 1]);
+    }
+
+    #[Route("/api/deck/draw/{num<\d+>}", name: "apiDeckDraw")]
+    public function apiDeckDraw(
+        int $num,
+        SessionInterface $session
+    ): Response
+    {
+        
+        $deck = $this->getDeck($session);
+
+        $drawnCards = array();
+
+        for ($i = 0; $i < $num; $i++) {
+            $drawnCards[] = $deck->drawCard()->getAsString();
+        }
+
+        $data = [
+            'cards' => $drawnCards
+        ];
         return new JsonResponse($data);
     }
 }
